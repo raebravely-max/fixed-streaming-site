e5c1324ee56c9fbf45e669a4e0fbdf8a834f8fe8
VDJ BRAVE <mac@VDJ-BRAVE.bbrouter>
2026-05-16 16:53:26 +0300

    Fix missing Home import

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header"; // ✅ FIXED (default import)
+import Header from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-import { PromoBanner } from "./components/PromoBanner";
-import { LiveEventCard } from "./components/LiveEventCard";
-import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="min-h-screen bg-[#0B0F19] text-white flex">
-
-      {/* Sidebar */}
       <Sidebar />
-
-      {/* Main Content */}
       <div className="flex-1 flex flex-col">
-
-        {/* Header */}
         <Header />
-
-        {/* Optional Promo Banner */}
-        <PromoBanner />
-
-        {/* Routes */}
         <div className="p-6">
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
+          <h1 className="text-2xl font-bold">Home</h1>
         </div>
-
       </div>
     </div>
   );
